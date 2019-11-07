bcfa31169807c15b3e5c4333151f9da7c1378ac4
Reed Jones <reed.jones@pderas.com>
2019-11-06 23:17:38 -0700

    now will generate mutations and install axios interceptors if available for auto vuex updates

--- src/index.js
+++ src/index.js
@@ -9,19 +9,63 @@ export * from 'vuex'
  * })
  */
 let options = {
-    // no options available
+    mutations: true,
+    axios: null
 }
 
+const generateMutations = state => Object.keys(state).reduce((acc, key) => ({ ...acc, [`X_SET_${key.toUpperCase()}`]: new Function('state', 'val', `state.${key} = val`) }), {})
 
 /**
  * Extend the base Vuex store class to pull window.__INITIAL_STATE__ by default
  */
 export class Store extends Vuex.Store {
     constructor(vuexState = {}, SSRState = window.__INITIAL_STATE__) {
-        let newState = objectMerge(vuexState, SSRState)
+        // generate base
+        if (SSRState.state) {
+            SSRState.mutations = generateMutations(SSRState.state)
+        }
+
+        for (let [key, data] of Object.entries(SSRState.modules)) {
+            if (data.state) {
+                SSRState.modules[key].mutations = generateMutations(data.state)
+            }
+        }
+
+        const newState = objectMerge(vuexState, SSRState)
 
         // with our data now formatted and merged, use Vuex's original constructor
         super(newState);
+
+        // find axios
+        const axios = options.axios || window.axios;
+
+        if (axios && options.mutations) {
+            // register automatic mutation interceptors
+            axios.interceptors.response.use(response => {
+                if (response.data && response.data.vuex) {
+                    if (response.data.vuex.state) {
+                        for (let [key, value] of Object.entries(response.data.vuex.state)) {
+                            this.commit(`X_SET_${key.toUpperCase()}`, value)
+                        }
+                    }
+
+                    if (response.data.vuex.modules) {
+                        for (let [name, data] of Object.entries(response.data.vuex.modules)) {
+                            if (data.state) {
+                                for (let [key, value] of Object.entries(data.state)) {
+                                    const mutation = newState.modules[name].namespaced ? `${name}/X_SET_${key.toUpperCase()}` : `X_SET_${key.toUpperCase()}`
+                                    this.commit(mutation, value)
+                                }
+                            }
+                        }
+                    }
+                }
+
+                return response;
+              }, error => {
+                return Promise.reject(error);
+              });
+        }
     }
 }
 
